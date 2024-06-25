bb9a1469f3b2d58686f4d5a6d9370fe9ce0afb00
Eric Vollenweider <eric.vollenweider@swiss-mile.com>
2024-06-25 13:35:24 +0200

    Fix install directory

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,3 +1,4 @@
 pip install torch==2.1.2
 pip install mmcv==2.1.0 -f https://download.openmmlab.com/mmcv/dist/cu118/torch2.1/index.html
-pip install -e .
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+pip install -e $SCRIPT_DIR
